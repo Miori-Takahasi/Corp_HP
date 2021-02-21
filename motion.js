22cc7de1da5b12c9b304de98d43b3d1e3773f285
Miori <tkhs21202@gmail.com>
2021-02-21 09:17:54 +0900

    ハンバーガーメニュー実装に成功！

--- motion.js
+++ motion.js
@@ -2,13 +2,15 @@
 $(function() {
 
 	$('#message').hide().fadeIn(1700);
-	
+
 	$('.header-icon').on('click', function() {
 		$(this).toggleClass('isClose');
 		if($(this).hasClass('isClose')) {
 			$('#header-icon-image').attr('src','images/ham_close.svg');
+			$('.menu-contents').slideDown();
 		} else {
 			$('#header-icon-image').attr('src','images/hamburger.svg');
+			$('.menu-contents').slideUp();
 		}
 	});
 
